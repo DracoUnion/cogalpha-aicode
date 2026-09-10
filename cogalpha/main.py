4427457407431b697f682d2e6497393eb0814513
wizardforcel <562826179@qq.com>
2026-09-11 04:05:57 +0800

    2026-09-11 04:05:57

--- cogalpha/main.py
+++ cogalpha/main.py
@@ -307,10 +307,9 @@ class CogAlpha:
         for h in hdls:
             parent_pool += h.result()
         hdls = []
-        
-
-        parent_pool = utils.rank_factors(parent_pool)[: self.cfg.generation.parent_pool_size]
         utils.write_yaml(parent_pool_fname, parent_pool)
+  
+        parent_pool = utils.rank_factors(parent_pool)[: self.cfg.generation.parent_pool_size]
         result.candidates = list(parent_pool)
         result.elite = list(parent_pool)
         self._step(f"1", "初始父池完成：%d 个因子", len(parent_pool))
